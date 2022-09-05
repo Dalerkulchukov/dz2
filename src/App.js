051013c70e48c707d586e4b5e46e6e7715ac962f
DalerKulchukov <laxusdreyar20020709@gmail.com>
2022-09-05 22:46:07 +0600

    dz2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AboutPages from "./pages/AboutPages/AboutPages";
+
 
 function App() {
+  const info = {
+    title: "I am title",
+    body: "Lorem inpsum bla bla bla"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AboutPages info={info}/>
     </div>
   );
 }
---
+++ src/pages/AboutPages/AboutPages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AboutPages({info}){
+    return(
+    <>
+        <h1>{info.title}</h1>
+        <p>{info.body}</p>
+    </>
+    )
+}
+export default AboutPages;
